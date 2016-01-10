fa361a05429ba467dedff39d17c27948783c8688
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2016-01-10 22:03:34 +0000

    Template changes to show new data

--- icw/web/static/js/main.js
+++ icw/web/static/js/main.js
@@ -2,14 +2,27 @@
     var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	var osmAttrib='Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors';
 	var osm = new L.TileLayer(osmUrl, {minZoom: 3, maxZoom: 19, attribution: osmAttrib});
-
+    var googleStreets = L.tileLayer('http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    });
+    var googleSat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+        maxZoom: 20,
+        subdomains:['mt0','mt1','mt2','mt3']
+    });
     var mapElems = document.getElementsByClassName('map');
     for (var i=0; i < mapElems.length; i++) {
         var mapElem = mapElems.item(i);
         var latLng = new L.LatLng(mapElem.dataset.lat, mapElem.dataset.lon);
-        map = L.map(mapElem);
+        map = L.map(mapElem, {
+            layers: [osm]
+        });
         map.setView(latLng, 14);
-        map.addLayer(osm);
+        L.control.layers({
+            "OpenStreetMap": osm,
+            "Google Streets": googleStreets,
+            "Google Satellite": googleSat
+        }).addTo(map);
         var marker = L.marker(latLng).addTo(map);
     }
 })();
